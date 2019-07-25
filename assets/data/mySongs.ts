6e3c75ebadcc0c5167e270464a78106a4e9ac12a
alvin248 <49872756+alvin248@users.noreply.github.com>
2019-07-25 11:42:16 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,9 @@
+import { Song } from '../../app/songInfo';
+
+export const MYSONGS : Song[] = [
+    { song: 'Money Made Me Do it', artist: 'Post Malone', genre: 'Hip-Hop/RnB', year: '2018', picture: 'pm.png' },
+    { song: 'What They Want', artist: 'Russ', genre: 'Hip-Hop/Rap', year: '2017', picture: 'russ.png' },
+    { song: 'Holy Mountain', artist: 'DJ Khaled', genre: 'Hip-Hop/Rap', year: '2019', picture: 'dj.png' },
+    { song: 'Night Job', artist: 'Bas feat. J.Cole', genre: 'Hip-Hop/Rap', year: '2019', picture: 'cole.png' }
+];
+
---
+++ assets/data/myalvin.ts
@@ -0,0 +1,5 @@
+import{ Aloraa } from "../../app/aloraa";
+
+export const ACLASS : Aloraa[] = [
+    { stdnum : '991172509', stdname : 'Alvin Alora', stdlogin : 'aloraa', campus : 'Trafalgar', assignTitle: 'Assignment 3' }
+];
