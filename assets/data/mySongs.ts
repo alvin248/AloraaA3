b4e039c6d8094731f675f613281198643204d65d
alvin248 <49872756+alvin248@users.noreply.github.com>
2019-07-26 14:52:21 -0400

    Add files via upload

--- assets/data/mySongs.ts
+++ assets/data/mySongs.ts
@@ -1,9 +1,9 @@
 import { Song } from '../../app/songInfo';
 
 export const MYSONGS : Song[] = [
-    { song: 'Money Made Me Do it', artist: 'Post Malone', genre: 'Hip-Hop/RnB', year: '2018', picture: '/assets/images/pm.png' },
-    { song: 'What They Want', artist: 'Russ', genre: 'Hip-Hop/Rap', year: '2017', picture: '/assets/images/russ.png' },
-    { song: 'Holy Mountain', artist: 'DJ Khaled', genre: 'Hip-Hop/Rap', year: '2019', picture: '/assets/images/dj.png' },
-    { song: 'Night Job', artist: 'Bas feat. J.Cole', genre: 'Hip-Hop/Rap', year: '2019', picture: '/assets/images/cole.png' }
+    { song: 'Money Made Me Do it', artist: 'Post Malone', genre: 'Hip-Hop/RnB', year: '2018', picture: './assets/images/pm.png' },
+    { song: 'What They Want', artist: 'Russ', genre: 'Hip-Hop/Rap', year: '2017', picture: './assets/images/russ.png' },
+    { song: 'Holy Mountain', artist: 'DJ Khaled', genre: 'Hip-Hop/Rap', year: '2019', picture: './assets/images/dj.png' },
+    { song: 'Night Job', artist: 'Bas feat. J.Cole', genre: 'Hip-Hop/Rap', year: '2019', picture: './assets/images/cole.png' }
 ];
 
